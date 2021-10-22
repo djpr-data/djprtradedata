6c268201e90d59f0622e6132d8b559eae1223c94
Matt Cowgill <mattcowgill@gmail.com>
2021-10-22 20:52:05 +1100

    add check_local logic to read_merch

--- R/read_merch.R
+++ R/read_merch.R
@@ -7,8 +7,11 @@
 #' ABS.Stat API. For this reason, you cannot download more than 1 year at a
 #' time worth of data using this function, as this is around the point at which
 #' the 1m row limit is reached.
+#' @param path Path to directory where XML files should be stored
 #' @param min_date The minimum date to include in your data
 #' @param max_date The maximum date to include in your data
+#' @param check_local Check if a local version of the requested data is
+#' available at the `path` location; if present it will be loaded.
 #' @examples
 #' \dontrun{
 #' read_merch()
@@ -17,25 +20,37 @@
 #' @return A tibble containing merchandise export data
 
 
-read_merch <- function(min_date = max_date - 365,
-                       max_date = Sys.Date()) {
+read_merch <- function(path = tempdir(),
+                       min_date = max_date - 365,
+                       max_date = Sys.Date(),
+                       check_local = TRUE) {
   if (max_date - min_date > 365) {
     stop("Cannot download more than 12 months worth of data at a time due to ABS limits.")
   }
 
+  min_month <- format(min_date, "%Y-%m")
+  max_month <- format(max_date, "%Y-%m")
+
   url <- paste0(
     "https://stat.data.abs.gov.au/restsdmx/sdmx.ashx/GetData/MERCH_EXP/-+2/all?startTime=",
-    format(min_date, "%Y-%m"),
+    min_month,
     "&endTime=",
-    format(max_date, "%Y-%m")
+    max_month
   )
 
-  file <- tempfile(fileext = ".xml")
+  file <- file.path(path,
+            paste0("abs_merch_", min_month, "_", max_month, ".xml"))
 
-  utils::download.file(url,
-    file,
-    mode = "wb"
-  )
+  if (isFALSE(check_local) || !file.exists(file)) {
+    message("Downloading merchandise trade data from ", min_month, " to ",
+            max_month)
+    utils::download.file(url,
+                         file,
+                         mode = "wb"
+    )
+  } else {
+    message("Loading merchandise trade from local file:\n", file)
+  }
 
   merch <- readsdmx::read_sdmx(file) %>%
     dplyr::as_tibble()
--- tests/testthat/test-read_merch.R
+++ tests/testthat/test-read_merch.R
@@ -1,6 +1,18 @@
 test_that("read_merch() works", {
-  m <- read_merch(min_date = Sys.Date() - 90)
+  skip_if_offline()
+
+  dir <- tempdir()
+  m <- read_merch(min_date = Sys.Date() - 90,
+                  path = dir)
+
   expect_s3_class(m, "tbl_df")
   expect_length(m, 10)
   expect_gte(nrow(m), 150000)
+  expect_message(read_merch(min_date = Sys.Date() - 90,
+                            path = dir),
+                 "local file")
+  expect_message(read_merch(min_date = Sys.Date() - 90,
+                            path = dir,
+                            check_local = FALSE),
+                 "Downloading")
 })
