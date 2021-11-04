1ac851c1472a0c20b12357ec225bc3a97a57228a
Louis-DJPR <louis.cui@ecodev.vic.gov.au>
2021-11-04 12:33:10 +1100

    editing test file to reflect new function read_supp()

--- tests/testthat/test-read_supp.R
+++ tests/testthat/test-read_supp.R
@@ -1,5 +1,5 @@
-test_that("read_supp_*() produce expected output", {
-  all_fy <- read_supp_fy()
+test_that("read_supp() produce expected output", {
+  all_fy <- read_supp()
 
   expect_type(all_fy, "list")
   lapply(all_fy, expect_s3_class, class = "tbl_df")
